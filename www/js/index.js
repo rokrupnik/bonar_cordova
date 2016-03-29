34a9e5865237588b3599dce63be2fca4178fe91a
rokrupnik <rok7rupnik@gmail.com>
2016-03-29 23:54:15 +0200

    Loaded restaurants data from a json file. This closes #4

--- www/js/index.js
+++ www/js/index.js
@@ -18,25 +18,26 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
+        app.loadData();
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -45,6 +46,18 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    // Load data
+    loadData: function () {
+        $.getJSON('js/restaurants.json')
+            .then(function (data) {
+                app.updateStatus('Number of loaded restaurants: ' + data.length);
+            });
+    },
+
+    updateStatus: function (status) {
+        $('#status').text(status);
     }
 };
 
