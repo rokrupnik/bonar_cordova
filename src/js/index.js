c0e09b939565fbbe26c097f8200e64a922b6dde3
gareins <ozbolt.menegatti@gmail.com>
2016-04-27 00:36:36 +0200

    Organizing index.js logically
    Also, added boder to active table.

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,12 @@ var libs = require('Libraries');
 var $ = libs.$;
 var _ = libs._;
 var ol = libs.ol;
-//var tl = libs.tl;
 
-var clusterSource;
-
-var app = {	
+// Main object
+var app = 
+{	
     // Application Constructor
     initialize: function () {
-        this.bindEvents();
-    },
-    
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     
@@ -23,15 +16,27 @@ var app = {
         // First start loading data
         app.loadData();
         
-        // Init everything else
-        app.initVariables();
+        // Init other stuff
+        app.display.init();
         app.map.init();
-        app.InitFooterImgClick();
-        app.geolocation();
-        app.switchTab("map");
+        app.geolocation.init();
+        app.settings.init();
         
-        // Hopefully this will correctly reset the map
+        // Lastly resize the map -> hopefully this resets the map correctly
         app.map.initResizer();
+    },
+        
+    // Load data
+    loadData: function () {
+        $.getJSON('js/restaurants.json')
+        .done(function (data) { 
+            app.restaurants = data.restaurants;
+            app.cities = data.cities;
+            app.restaurantFeatures = data.features;
+            
+            // After data loaded:
+            app.onDataLoaded();
+        });
     },
     
     // Run after all data in app.loadData is loaded
@@ -39,70 +44,77 @@ var app = {
         app.map.loadRestaurants();
     },
     
-    initVariables: function() {
-        var fh = $(".site-footer").css("height");
-        app.footerHeight = parseInt(fh.substring(0, fh.length - 2));
+    display: {
+        init: function() {
+            // Get variables
+            var fh = $(".site-footer").css("height");
+            app.footerHeight = parseInt(fh.substring(0, fh.length - 2));
+            
+            // footer image click
+            $(".footer-img").on("click", function() {
+                app.display.switchTab($(this).attr("name"));
+            });
+            
+            // switch to default tab
+            app.display.switchTab("map");
+        },
+        
+        switchTab: function (tab) {
+            $(".tab").css("display", "none");
+            $("#tab-" + tab).css("display", "initial");
+            
+            $(".tab-active").removeClass("tab-active");
+            $(".footer-img[name='" + tab + "']").addClass("tab-active");
+        }
     },
     
-    // Get location
-    geolocation: function() {
-        var onSuccess = function(position) {
-            alert('Latitude: '          + position.coords.latitude          + '\n' +
-                  'Longitude: '         + position.coords.longitude         + '\n' +
-                  'Altitude: '          + position.coords.altitude          + '\n' +
-                  'Accuracy: '          + position.coords.accuracy          + '\n' +
-                  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                  'Heading: '           + position.coords.heading           + '\n' +
-                  'Speed: '             + position.coords.speed             + '\n' +
-                  'Timestamp: '         + position.timestamp                + '\n');
-        };
+    settings: {
+        //TODO...
+        init: function() {}
+        // getter: function(setting) {},
+        // setter: function(setting, value) {}
+    },
+    
+    geolocation: {
+        onSuccess: function(position) {
+            app.locationData.err  = null;
+            app.locationData.lat  = position.coords.latitude;
+            app.locationData.lon  = position.coords.longitude;
+            app.locationData.acc  = position.coords.accuracy;
+            app.locationData.time = position.timestamp;
+        },
 
-        var onError = function(error) {
-            alert('code: '    + error.code    + '\n' +
-                  'message: ' + error.message + '\n');
-        };
+        onError: function(error) {
+            app.locationData.err = error.message; //err.code needed?
+        },
 
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    },
-    
-    // Load data
-    loadData: function () {
-        $.getJSON('js/restaurants.json')
-        .done(function (data) { 
-            app.restaurants = data.restaurants; 
-            app.onDataLoaded();
-        });
-    },
-    
-    switchTab: function (tab) {
-        $(".tab").css("display", "none");
-        $("#tab-" + tab).css("display", "initial");
-    },
-    
-    InitFooterImgClick: function() {
-        $(".site-footer ul li img").on("click", function() {
-            app.switchTab($(this).attr("name"));
-        });
+        getter: function() {
+            // check settings...
+            
+            // Get location
+            navigator.geolocation.getCurrentPosition(app.geolocation.onSuccess, app.geolocation.onError);
+        },
+        
+        init: function() { app.geolocation.getter() }
     },
     
     map: {
         init: function () {
-            
-            app.map.vectorSource = new ol.source.Vector();
+            app.vectorSource = new ol.source.Vector();
 
-            clusterSource = new ol.source.Cluster({
-                source: app.map.vectorSource,
+            app.clusterSource = new ol.source.Cluster({
+                source: app.vectorSource,
                 distance: 20
             });
 
-            app.map.map = new ol.Map({
+            app.bigmap = new ol.Map({
                 target: 'mapid',
                 layers: [
                     new ol.layer.Tile({
                         source: new ol.source.MapQuest({layer: 'osm'})
                     }),
                     new ol.layer.Vector({
-                        source: clusterSource,
+                        source: app.clusterSource,
                         style: app.map.restaurantStyleFun
                     })
                 ],
@@ -110,7 +122,7 @@ var app = {
                 interactions: ol.interaction.defaults({altShiftDragRotate:false, pinchRotate:false})
             });
 
-            app.map.map.setView(new ol.View({
+            app.bigmap.setView(new ol.View({
                 center: ol.proj.fromLonLat([14.514713, 46.0565274]),
                 zoom: 13
             }));
@@ -121,7 +133,7 @@ var app = {
                 positioning: 'bottom-center',
                 stopEvent: false
             });
-            app.map.map.addOverlay(popup);
+            app.bigmap.addOverlay(popup);
             $(element).popover({
                 'placement': 'top',
                 'html': true,
@@ -131,8 +143,8 @@ var app = {
             });
 
             // display popup on click
-            app.map.map.on('click', function(evt) {
-                var feature = app.map.map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+            app.bigmap.on('click', function(evt) {
+                var feature = app.bigmap.forEachFeatureAtPixel(evt.pixel, function(feature) {
                     return feature;
                 });
                 if (feature) {
@@ -145,14 +157,14 @@ var app = {
             });
 
             // change mouse cursor when over marker
-            $(app.map.map.getViewport()).on('mousemove', function(e) {
+            $(app.bigmap.getViewport()).on('mousemove', function(e) {
                 if (e.dragging) {
                     $(element).popover('hide');
                     return;
                 }
-                var pixel = app.map.map.getEventPixel(e.originalEvent);
-                var hit = app.map.map.hasFeatureAtPixel(pixel);
-                app.map.map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+                var pixel = app.bigmap.getEventPixel(e.originalEvent);
+                var hit = app.bigmap.hasFeatureAtPixel(pixel);
+                app.bigmap.getTargetElement().style.cursor = hit ? 'pointer' : '';
             });
 
             // hide popover on zoom - not working right now
@@ -230,7 +242,7 @@ var app = {
                 var mapel = $(".app");                
                 mapel.css("height", $("body").height() - app.footerHeight);
                 mapel.css("width", $("body").width());
-                app.map.map.updateSize();
+                app.bigmap.updateSize();
             };
             
             resizeFun()
@@ -260,10 +272,25 @@ var app = {
 
                 feat.set('opening', opening);
 
-                app.map.vectorSource.addFeature(feat);
+                app.vectorSource.addFeature(feat);
             });
         }
-    }
+    },
+    
+    // explicitally declared variables:
+    bigmap:             null,
+    smallmap:           null,
+    vectorSource:       null,
+    clusterSource:      null,
+    
+    restaurants:        null,
+    cities:             null,
+    restaurantFeatures: null,
+    
+    footerHeight:       null,
+    locationData:       {},
+    settingsData:       {}
 };
 
+// "main()"
 app.initialize();
